6dc3be46249c6ffbca0c73e24acb1e12bf675fee
RylanHiltz <rylanhiltz2@gmail.com>
2024-05-21 22:27:34 -0400

    Add featured info section (experience, location, socials)

---
+++ app/components/FeaturedInfo.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+
+const InfoBar = () => {
+  return (
+    <div className="grid grid-cols-6 grid-rows-1 gap-5 my-20">
+      <div className="col-span-3">
+        <h2>Recent Experience</h2>
+        <p className="text-[#CACACA] text-sm font-light pt-1">
+          Freelance Web Developer with 10+ satisfied clients (cap){" "}
+        </p>
+      </div>
+
+      <div>
+        <h2>Location</h2>
+        <p className="text-[#CACACA] text-sm font-light pt-1">Ontario, CA </p>
+      </div>
+
+      <div className="col-span-2">
+        <h2>Socials</h2>
+        <div>
+          <ul className="flex flex-wrap gap-4">
+            <li>
+              <a
+                className=" text-[#CACACA] text-sm font-light pt-1"
+                href="https://github.com/RylanHiltz"
+                target="_blank"
+              >
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a
+                className=" text-[#CACACA] text-sm font-light pt-1"
+                href=""
+              >
+                LinkedIn
+              </a>
+            </li>
+            <li>
+              <a
+                className=" text-[#CACACA] text-sm font-light pt-1"
+                href=""
+              >
+                Dribbble
+              </a>
+            </li>
+            <li>
+              <a
+                className=" text-[#CACACA] text-sm font-light pt-1"
+                href=""
+              >
+                Awwwards
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InfoBar;
