b5783653136e79d8d09dff6f957f61927f7a1dc9
RylanHiltz <rylanhiltz2@gmail.com>
2024-05-23 17:04:33 -0400

    Add navbar component

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+
+const NavBar = () => {
+  return (
+    <div className="flex bg-white bg-opacity-20 shadow-md shadow-black/[0.03] backdrop-blur-[0.3rem] fixed bottom-20 justify-center self-center rounded-2xl h-full w-full max-w-[350px] max-h-[65px]">
+      
+    </div>
+  );
+};
+
+export default NavBar;
