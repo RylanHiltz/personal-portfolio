7b30ff4c086c07eead2cdff246bc03b4c5509520
RylanHiltz <rylanhiltz2@gmail.com>
2024-05-25 22:51:31 -0400

    Add navbar links to data file

---
+++ app/lib/data.ts
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
