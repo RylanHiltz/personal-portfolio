d75a9bd0bdcbce8ea458fc3a488853155e9a19f3
RylanHiltz <rylanhiltz2@gmail.com>
2024-05-26 23:28:01 -0400

    Add about me section to page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import FeaturedInfo from "./components/FeaturedInfo";
 import NavBar from "./components/NavBar";
+import Aboutme from "./components/Aboutme";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <HeroSection></HeroSection>
       <FeaturedInfo></FeaturedInfo>
       <NavBar></NavBar>
+      <Aboutme></Aboutme>
     </main>
   );
 }
