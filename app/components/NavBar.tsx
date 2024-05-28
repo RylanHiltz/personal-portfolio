514729198f8ca24e5dbabb430c7716db07530cc1
RylanHiltz <rylanhiltz2@gmail.com>
2024-05-27 23:58:23 -0400

    Fix navbar blur hover issue

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -5,10 +5,12 @@ import { motion } from "framer-motion";
 import { links } from "../lib/data";
 import Link from "next/link";
 
+
+
 export default function NavBar() {
   return (
     <div className="flex justify-center self-center">
-      <div className=" bg-gradient-to-r from-[#C3C3C3]/30 to-[#5D5D5D]/15 bg-opacity-20 shadow-md shadow-black/[0.03] backdrop-blur-[0.3rem] fixed bottom-20  rounded-2xl h-full w-full max-w-[350px] max-h-[65px] justify-center px-3 flex">
+      <div className="z-50 bg-gradient-to-r from-[#C3C3C3]/30 to-[#5D5D5D]/15 shadow-md shadow-black/[0.03] fixed bottom-12  backdrop-blur-[0.3rem] bg-opacity-20 rounded-2xl h-full w-full max-w-[350px] max-h-[65px] justify-center px-3 flex">
         <ul className="flex items-center justify-center gap-6">
           {links.map((link) => (
             <motion.li
