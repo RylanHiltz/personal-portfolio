282cac86e8ec22193ce1069f53e1f98302ddcad3
RylanHiltz <rylanhiltz2@gmail.com>
2024-06-08 22:45:47 -0400

    Add project data to data file

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -18,3 +18,38 @@ export const links = [
     hash: "#contact",
   },
 ] as const;
+
+// TODO: make list of project data for exporting [name, date, description, href] for each project
+export const projectsData = [
+  {
+    title: "Project 1",
+    description: "this is a description",
+    tags: ["React", "TypeScript", "Tailwind CSS"]
+  },
+  {
+    title: "Project 2",
+    description: "this is a description",
+    tags: ["React", "TypeScript", "Tailwind CSS"]
+  },
+  {
+    title: "Project 3",
+    description: "this is a description",
+    tags: ["React", "TypeScript", "Tailwind CSS"]
+  },
+  {
+    title: "Project 4",
+    description: "this is a description",
+    tags: ["React", "TypeScript", "Tailwind CSS"]
+  },
+  {
+    title: "Project 5",
+    description: "this is a description",
+    tags: ["React", "TypeScript", "Tailwind CSS"]
+  },
+  {
+    title: "Project 6",
+    description: "this is a description",
+    tags: ["React", "TypeScript", "Tailwind CSS"]
+  }
+] as const;
+
