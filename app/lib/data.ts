b95307e349867f032770db4e95efabaf64adfef0
RylanHiltz <rylanhiltz2@gmail.com>
2024-06-11 18:04:37 -0400

    Changes to project data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -22,33 +22,33 @@ export const links = [
 // TODO: make list of project data for exporting [name, date, description, href] for each project
 export const projectsData = [
   {
-    title: "Project 1",
-    description: "this is a description",
-    tags: ["React", "TypeScript", "Tailwind CSS"]
+    title: "Figma iPhone Icon Pack ",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
+    tags: ["React", "TypeScript", "Tailwind CSS"],
   },
   {
     title: "Project 2",
-    description: "this is a description",
+    description: "this is a description for the 2nd project :O",
     tags: ["React", "TypeScript", "Tailwind CSS"]
   },
   {
     title: "Project 3",
-    description: "this is a description",
+    description: "this is a description for the 3rd project :)",
     tags: ["React", "TypeScript", "Tailwind CSS"]
   },
   {
     title: "Project 4",
-    description: "this is a description",
-    tags: ["React", "TypeScript", "Tailwind CSS"]
+    description: "this is a description for the 4th project :(",
+    tags: ["React", "TypeScript", "Tailwind CSS"],
   },
   {
     title: "Project 5",
-    description: "this is a description",
+    description: "this is a description for the 5th project :D",
     tags: ["React", "TypeScript", "Tailwind CSS"]
   },
   {
     title: "Project 6",
-    description: "this is a description",
+    description: "this is a description for the 6th project :C",
     tags: ["React", "TypeScript", "Tailwind CSS"]
   }
 ] as const;
