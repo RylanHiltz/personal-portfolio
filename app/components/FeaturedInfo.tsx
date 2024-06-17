d285c58fb1c3ee45d8c26deb12f28c9614f5193e
RylanHiltz <rylanhiltz2@gmail.com>
2024-06-17 00:06:08 -0400

    Add tablet responsive design to featured info section

--- app/components/FeaturedInfo.tsx
+++ app/components/FeaturedInfo.tsx
@@ -6,11 +6,12 @@ import SocialButton from "./SocialButton";
 
 export default function InfoBar() {
   return (
-    <div className="grid grid-cols-6 grid-rows-1 gap-5 my-20">
-      <div className="col-span-3">
+    // TODO: change "mobile-lg" to tablet after making screen widths for resposiveness
+    <div className="grid grid-cols-6 grid-rows-1 gap-5 my-20 max-mobile-lg:grid-flow-row max-mobile-lg:max-w-[425px] max-mobile-lg:grid-cols-4 ">
+      <div className="col-span-3 max-mobile-lg:col-span-4">
         <h2>Recent Experience</h2>
         <p className="text-[#CACACA] text-sm font-light pt-1">
-          Freelance Web Developer with 10+ satisfied clients (cap){" "}
+          Freelance Web Developer with 10+ satisfied clients{" "}
         </p>
       </div>
 
@@ -19,7 +20,7 @@ export default function InfoBar() {
         <p className="text-[#CACACA] text-sm font-light pt-1">Ontario, CA </p>
       </div>
 
-      <div className="col-span-2">
+      <div className="col-span-2 justify-self-end max-mobile-lg:col-span-3 max-mobile-lg:justify-self-start">
         <h2>Socials</h2>
         <div>
           <ul className="flex flex-wrap gap-4">
